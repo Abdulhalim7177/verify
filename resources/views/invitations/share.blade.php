cc9f66ce79d8aaff5f1e3d2f8785de6fab56fa4e
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-03-17 13:19:17 +0100

    Replace Bootstrap CDN with Vite for asset management in share.blade.php

--- resources/views/invitations/share.blade.php
+++ resources/views/invitations/share.blade.php
@@ -1,4 +1,5 @@
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@vite(['resources/sass/app.scss', 'resources/js/app.js'])
+
 <div class="container mt-5">
     <div class="row justify-content-center">
         <div class="col-md-6">
