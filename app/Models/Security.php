3925c2c026de377f366ac2f2071fee8e770c183f
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-04-15 12:48:48 +0100

    Remove unused HasApiTokens trait from Security model and clarify guard specification

--- app/Models/Security.php
+++ app/Models/Security.php
@@ -5,12 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Model;
 
 class Security extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
+    protected $guard = 'security'; // Specify the guard
     /**
      * The attributes that are mass assignable.
      *
