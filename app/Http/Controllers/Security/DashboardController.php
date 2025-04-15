82dc41f7e4b931b713068b234541f87875cec913
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-04-15 12:34:20 +0100

    Add DashboardController with authentication middleware and dashboard view

--- app/Http/Controllers/Security/DashboardController.php
+++ app/Http/Controllers/Security/DashboardController.php
@@ -7,5 +7,23 @@ use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:security');
+    }
+
+    /**
+     * Show the security dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('security.dashboard');
+    }
 }
