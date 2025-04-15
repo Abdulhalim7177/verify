52a5a4c9368a71a50da6843232309a4867f1db0b
Abdulhalim7177 <abdulmuhd7177@gmail.com>
2025-04-15 12:33:42 +0100

    Implement LoginController and Security model with authentication features and migration updates

--- database/migrations/2025_04_15_112933_create_securities_table.php
+++ database/migrations/2025_04_15_112933_create_securities_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('securities', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('phone_number')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->rememberToken();
             $table->timestamps();
         });
     }
